e052604004905dd5b42bedcae5c134708200be5e
Phạm Nguyên Thủy <phamnguyenthuy.com@gmail.com>
2025-10-24 22:19:40 +0700

    Update header.js

--- assets/js/components/header.js
+++ assets/js/components/header.js
@@ -1,3 +1,5 @@
 fetch("components/header.html")
   .then(res => res.text())
-  .then(data => document.getElementById("header").innerHTML = data);
+  .then(data => {
+    document.getElementById("header").innerHTML = data;
+  });
