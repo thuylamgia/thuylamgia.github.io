46a9099b4e2deab3bcc939273099214e81ba3d1f
Phạm Nguyên Thủy <phamnguyenthuy.com@gmail.com>
2025-10-24 23:52:16 +0700

    Update contact.js

--- assets/js/components/contact.js
+++ assets/js/components/contact.js
@@ -1,3 +1,6 @@
 fetch("components/contact.html")
-  .then(res => res.text())
-  .then(data => document.getElementById("contact").innerHTML = data);
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("contact").innerHTML = data;
+  })
+  .catch(err => console.error("Không thể tải contact:", err));
