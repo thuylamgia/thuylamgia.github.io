5baa2ece9926d7bcb04c8bb4d1dda15865c5c0af
Phạm Nguyên Thủy <phamnguyenthuy.com@gmail.com>
2025-10-24 21:30:44 +0700

    Update header.js

--- assets/js/components/header.js
+++ assets/js/components/header.js
@@ -1,13 +1,5 @@
 fetch("components/header.html")
-  .then(res => res.text())
+  .then(response => response.text())
   .then(data => {
     document.getElementById("header").innerHTML = data;
-
-    // Sau khi header được nạp, nạp tiếp menu
-    fetch("components/menu.html")
-      .then(res => res.text())
-      .then(menu => {
-        // Thêm menu ngay bên dưới header
-        document.getElementById("header").insertAdjacentHTML("beforeend", menu);
-      });
   });
