0f937548f16e53fbead9ea2bfe1d70f4d364eb95
Phạm Nguyên Thủy <phamnguyenthuy.com@gmail.com>
2025-10-24 20:43:48 +0700

    Update footer.js

--- assets/js/components/footer.js
+++ assets/js/components/footer.js
@@ -1,8 +1,5 @@
-﻿document.addEventListener("DOMContentLoaded", () => {
-    fetch("components/footer.html")
-        .then(res => res.text())
-        .then(data => {
-            document.getElementById("footer").innerHTML = data;
-        })
-        .catch(err => console.error("Không thể tải footer:", err));
-});
+fetch("components/footer.html")
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById("footer").innerHTML = data;
+  });
