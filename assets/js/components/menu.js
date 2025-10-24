7e93dafa3811a83e3ec31fef049cc734870f7813
Phạm Nguyên Thủy <phamnguyenthuy.com@gmail.com>
2025-10-24 20:43:07 +0700

    Update menu.js

--- assets/js/components/menu.js
+++ assets/js/components/menu.js
@@ -1,16 +1,5 @@
-document.addEventListener("DOMContentLoaded", function () {
-  fetch("menu.html")
-    .then(response => response.text())
-    .then(data => {
-      // Tạo phần tử chứa menu
-      const navContainer = document.createElement("div");
-      navContainer.innerHTML = data;
-      
-      // Chèn menu ngay sau header
-      const header = document.getElementById("header");
-      if (header) {
-        header.insertAdjacentElement("afterend", navContainer);
-      }
-    })
-    .catch(error => console.error("Không thể tải menu:", error));
-});
+fetch("components/menu.html")
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById("menu").innerHTML = data;
+  });
