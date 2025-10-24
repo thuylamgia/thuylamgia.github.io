855d8b4df776ef779d787c8e29e85030cc4a539b
Phạm Nguyên Thủy <phamnguyenthuy.com@gmail.com>
2025-10-24 20:42:45 +0700

    Update header.js

--- assets/js/components/header.js
+++ assets/js/components/header.js
@@ -1,8 +1,5 @@
-﻿document.addEventListener("DOMContentLoaded", () => {
-    fetch("components/header.html")
-        .then(res => res.text())
-        .then(data => {
-            document.getElementById("header").innerHTML = data;
-        })
-        .catch(err => console.error("Không thể tải header:", err));
-});
+fetch("components/header.html")
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById("header").innerHTML = data;
+  });
